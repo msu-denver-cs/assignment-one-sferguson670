6c4bb2a55e1518af03259824e08e090108efa3d7
sferguson670 <sferguson670@gmail.com>
2019-09-23 19:41:05 -0600

    updated part seeds
    auto-adds car parts for testing

--- hw1/db/seeds.rb
+++ hw1/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Part.delete_all
+#...
+Part.create(part_name: 'Windshield')
+#...
