dda7dff426c50bf40aa94e91b8bf916579ab0385
sferguson670 <sferguson670@gmail.com>
2019-09-28 22:54:42 -0600

    scaffold Car
    redid after fixing make scaffold

--- hw1/db/schema.rb
+++ hw1/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_045246) do
+ActiveRecord::Schema.define(version: 2019_09_29_045359) do
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.integer "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "makes", force: :cascade do |t|
     t.string "name"
