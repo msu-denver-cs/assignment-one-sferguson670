c520de896d8e01cff391ee4f8491c25343aaba5a
sferguson670 <sferguson670@gmail.com>
2019-09-28 22:53:29 -0600

    scaffold Make (redo)
    redid it with correct parameters

--- hw1/db/schema.rb
+++ hw1/db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_032749) do
+ActiveRecord::Schema.define(version: 2019_09_29_045246) do
+
+  create_table "makes", force: :cascade do |t|
+    t.string "name"
+    t.string "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
