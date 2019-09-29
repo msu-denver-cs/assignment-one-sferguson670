6b29c9fa64a403c9ad58b11badb73c4f0061cc9a
sferguson670 <sferguson670@gmail.com>
2019-09-28 21:33:33 -0600

    scaffold Make
    Make string:name string:country

--- hw1/db/schema.rb
+++ hw1/db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_032749) do
+ActiveRecord::Schema.define(version: 2019_09_29_033156) do
+
+  create_table "makes", force: :cascade do |t|
+    t.string "name"
+    t.string "model"
+    t.integer "vin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
